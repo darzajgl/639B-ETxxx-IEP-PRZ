8b9fb2cc1dfd113c44407e7718db3346a1207bcc
Dariusz Zajglic <darzajgl@gmail.com>
2021-12-20 01:46:15 +0100

    zadanie dodatkowe 2

---
+++ Z2_Zajglic/Zajglic_Dariusz_z2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+/*
+1. wymusza wpisanie liczby ca³kowitej x zawartej w przedziale <A,B>
+2. drukuje tak okreœlon¹ wartoœæ x
+3. wczytuje ci¹g liczb ca³kowitych a¿ do wczytania liczby parzystej lub podzielnej przez x
+4. drukuje najwiêksz¹ ujemn¹ liczbê tego ci¹gu (nie bior¹c pod uwagê liczby koñcz¹cej ci¹g)
+    - lub informacjê, ¿e nie by³o liczb ujemnych
+*/
+
+int main() {
+    //deklaracje
+    int A = -30;
+    int B = 30;
+    int x = A-1;
+    int zagadka;
+    int najwieksza_ujemna = 0;
+
+    //1.
+    while(x < A || x > B) {
+        cout << "\Podaj liczbe z przedzialu <"<<A<<","<<B<<">"<<endl;
+        cin >> x;
+    }
+
+    //2.
+    cout << "Swietnie, wprowadziles " << x << "." << endl;
+
+    //ponizsze rozwiazanie nie zadziala w przypadku x = 0
+
+    cout << "\nPora na zagadke. \nZnajdz liczbe, ktora uwolni Cie z petli."<<endl;
+
+    //3.
+    do {
+        if((zagadka < 0 ) && ((zagadka > najwieksza_ujemna) || (najwieksza_ujemna == 0))) {
+            najwieksza_ujemna = zagadka;
+        }
+        cout << "\nPodaj liczbe: ";
+        cin >> zagadka;
+        cout << "\nWpisales "<<zagadka<<"."<<endl;
+
+    } while((zagadka%2 != 0) && (zagadka%x != 0));
+
+    //4.
+    {
+        cout<< "Uwolniles sie z petli!"<<endl;
+        if(najwieksza_ujemna == 0) {
+            cout << "\nNie podales liczb ujemnych."<<endl;
+        } else {
+            cout << "\nNajwieksza ujemna podana przez Ciebie liczba to: " << najwieksza_ujemna <<endl;
+        }
+    }
+    return 0;
+}
