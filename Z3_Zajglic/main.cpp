44a1d1e116b86b33a2b06c033a20ddbbf570949a
Dariusz Zajglic <darzajgl@gmail.com>
2022-01-02 20:33:27 +0100

    zadanie dodatkowe 4

--- Z3_Zajglic/main.cpp
+++ Z3_Zajglic/main.cpp
@@ -19,88 +19,86 @@ int main() {
 
 //a.
 
-    int N = 6;
+    int N = 5;
 
-            if (N <= 1) {
-                cout << "N jest mniejsze lub rowne 1, niestety musze zakonczyc program. \nZmien wartosc N i uruchom program ponownie.\n"<<endl;
-                return 0;
-            }
+    if (N <= 1) {
+        cout << "N jest mniejsze lub rowne 1, niestety musze zakonczyc program. \nZmien wartosc N i uruchom program ponownie.\n"<<endl;
+        return 0;
+    }
 
 //b.
-            int a, bigger, bigest;
+    int a, bigger, bigest;
 
-            cout << "Podaj "<< N <<" liczb." << endl;
-
-
-            for (int i = 0; i <= N - 1; i++) {
-                cout << "Podaj wartosc liczbowa nr "<<i+1 <<" i wcisnij Enter :";
-                cin >> a;
+    cout << "Podaj "<< N <<" liczb." << endl;
 
 
-                if (i==0) {
-                    bigest = a;
+    for (int i = 0; i <= N - 1; i++) {
+        cout << "Podaj wartosc liczbowa nr "<<i+1 <<" i wcisnij Enter :";
+        cin >> a;
 
-                } else if(i == 1) {
 
-                    if(bigest < a) {
-                        bigger = bigest;
-                        bigest = a;
+        if (i==0) {
+            bigest = a;
 
-                    } else {
-                        bigger = a;
-                    }
+        } else if(i == 1) {
 
-                } else if (bigest < a) {
-                    bigger = bigest;
-                    bigest = a;
+            if(bigest < a) {
+                bigger = bigest;
+                bigest = a;
 
-                } else if(bigger < a) {
-                    bigger = a;
-                }
+            } else {
+                bigger = a;
+            }
 
-            };
+        } else if (bigest < a) {
+            bigger = bigest;
+            bigest = a;
 
-            cout << "\nNajwieksze z podanych liczb to: "<< bigest <<" oraz " << bigger <<".\n" << endl;
+        } else if(bigger < a) {
+            bigger = a;
+        }
+
+    };
+
+    cout << "\nNajwieksze z podanych liczb to: "<< bigest <<" oraz " << bigger <<".\n" << endl;
 
 //c.
 
-            string b, last, secondLast;
+    string b, last, secondLast;
 
-            cout << "Podaj "<< N <<" napisow." << endl;
+    cout << "Podaj "<< N <<" napisow." << endl;
 
-            for (int i = 0; i<= N-1; i++) {
-                cout << "Wprowadz napis nr " << i+1 << " i wcisnij Enter :";
-                cin >> b;
-                if(i==N-2) {
-                    secondLast = b;
-                }
-                if(i==N-1) {
-                    last = b;
-                }
-            };
-            cout << "\nOstatni z wprowadzonych napisow to \""<< last <<"\" a przedostatni to \"" <<secondLast<<"\".\n" << endl;
+    for (int i = 0; i<= N-1; i++) {
+        cout << "Wprowadz napis nr " << i+1 << " i wcisnij Enter :";
+        cin >> b;
+        if(i==N-2) {
+            secondLast = b;
+        }
+        if(i==N-1) {
+            last = b;
+        }
+    };
+    cout << "\nOstatni z wprowadzonych napisow to \""<< last <<"\" a przedostatni to \"" <<secondLast<<"\".\n" << endl;
 
 //d.
 
-    char arr[N/2];
-    char c;
+    char arr[N];
+
 
     cout << "Podaj " << N <<" znakow." << endl;
 
     for (int i = 0; i < N; i ++) {
         cout << "Wprowadz znak nr "<< i+1 <<" i wcisnij Enter :";
-        cin >> c;
-        if((i + N%2) %2 == 0) {
-            arr[i/2] = c;
-        }
+        cin >> arr[i];
     }
+
     cout << endl;
     cout << "Co drugi znak od konca: " << endl;
 
-    for (int i = 0; i < N/2; i++) {
-        cout << arr[N/2 - i - 1];
-
+    for (int i = N-1; i >= 0 ; i-=2) {
+        cout << arr[i];
     }
+
 
     return 0;
 }
---
+++ Z4_Zajglic/main.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+
+/*
+a. Tablice A[n][n] (n-stala) wypelnic liczbami losowymi calkowitymi z przedzialu <D,G> (D,G-stale)
+b. Wydrukowac tablice z wierszami ustawiona za pomoca stalej W szerokoscia wydruku.
+c. W dodatkowym wierszu pod tak wydrukowana tablica wydrukowac:
+    - pod kolumna o indeksie 0 - ilosc liczb parzystych w tej kolumnie
+    - pod kazda nastepna kolumna - roznice pomiedzy iloscia liczb parzystych w tej kolumnie a iloscia liczb parzystych w kolumnie poprzedniej
+d. Znalezc najwiekszy element glownej przekatnej w tablicy A i zapamietac oraz wydrukowac indeks wiersza w ktorym sie on znajduje.
+e. Zmienic miejscami znaleziony najwiekszy element z najmniejszym elementem wiersza, w ktorym go znaleziono.
+f. Wydrukowac zawartosc tego wiersza po dokonanej zmianie.
+
+*/
+
+using namespace std;
+
+int main() {
+
+    int n = 4;
+    int D = -17;
+    int G = 14;
+    int A[n][n];
+    int W = 7;
+
+//a.
+
+    for(int i = 0; i < n; i ++) {
+        for(int j = 0; j<n; j++) {
+            A[i][j]= D + rand()%(G+1-D);
+        }
+    }
+
+//b.
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << setw(W) << A[i][j];
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+//c.
+
+    int curr=0;
+    int prev=0;
+
+    for(int j = 0; j < n; j++) {
+        for(int i = 0; i < n; i++) {
+            if (A[i][j] % 2 == 0) {
+                curr++;
+            }
+        }
+        cout << setw(W) << (curr - prev);
+        prev = curr;
+        curr = 0;
+    }
+    cout<<endl;
+
+//d.
+
+    int maxDiag = A[0][0];
+    int maxDiagIndex = 0;
+
+    for(int i = 0; i < n; i++) {
+        if(A[i][i]>maxDiag) {
+            maxDiag = A[i][i];
+            maxDiagIndex = i;
+        }
+    }
+    cout << " \nNajwiekszy element na przekatnej to " << maxDiag << ", ktory znajduje sie w wierszu nr " << maxDiagIndex << "." <<endl;
+
+//e.
+
+    int minRow = A[maxDiagIndex][0];
+    int minColumnIndex = 0;
+
+    for(int j = 1 ; j < n; j++)
+        if(A[maxDiagIndex][j] < minRow) {
+            minRow = A[maxDiagIndex][j];
+            minColumnIndex = j;
+        }
+
+    int temp = A[maxDiagIndex][minColumnIndex];
+    A[maxDiagIndex][minColumnIndex] = A[maxDiagIndex][maxDiagIndex];
+    A[maxDiagIndex][maxDiagIndex] = temp;
+
+//f.
+
+    cout << "\nWiersz o indeksie " <<maxDiagIndex << " z zamienionymi wartosciami:" << endl;
+    for (int j= 0; j < n; j++) {
+        cout << setw(W) << A[maxDiagIndex][j];
+    }
+
+
+    return 0;
+}
